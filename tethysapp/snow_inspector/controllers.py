d928dc97ea8ab32364a1b6ceca5f8032f92d0d10
jirikadlec2 <jirikadlec2@gmail.com>
2015-07-23 17:36:18 -0600

    clean-up code and try to retrieve the favorite location from the DB

--- tethysapp/snow_inspector/controllers.py
+++ tethysapp/snow_inspector/controllers.py
@@ -19,18 +19,33 @@ def map(request):
     """
     Controller for map page.
     """
-    # Create a session
-    session = SessionMaker()
+
+    # If we received the input from the graph page:
+    if request.GET:
+        print 'request.GET!'
+        lat = request.GET['lat']
+        lon = request.GET['lon']
+        numdays = int(request.GET['days'])
+        endDate = request.GET['end']
+        today = endDate.strptime('%Y-%m-%d')
+        print 'days: ' + str(numdays)
+    else:
+        # Create a session
+        session = SessionMaker()
     
-    #Query DB for gage object
-    id = 1
-    site = session.query(SnowSite).filter(SnowSite.id==id).one()
+        #Query DB for gage object
+        id = 1
+        site = session.query(SnowSite).filter(SnowSite.id==id).one()
+        lat = site.latitude
+        lon = site.longitude
+        today = datetime.date.today()
+        numdays = 100
     
     #Transform into GeoJSON format
     geometries = []
     
     site_geometry = dict(type="Point",
-        coordinates=[site.latitude, site.longitude],
+        coordinates=[lat, lon],
                         properties={"value":site.value})
     geometries.append(site_geometry)
 
@@ -43,7 +58,7 @@ def map(request):
                    'input_overlays': geojson_sites}
 
     #configure the date picker
-    today = datetime.date.today()
+    
     date_picker = {'display_text': 'Date',
         'name': 'endDate',
         'autoclose': True,
@@ -54,17 +69,18 @@ def map(request):
     }
 
     days_picker = {'display_text': 'Number of days:',
-              'name': 'inputDays',
-              'placeholder': '100'}
+        'name': 'inputDays',
+        'placeholder': numdays
+    }
 
     # Pre-populate lat-picker and lon_picker from model
     lat_picker = {'display_text': 'Latitude:',
               'name': 'inputLat',
-              'placeholder': site.latitude}
+              'placeholder': lat}
 
     lon_picker = {'display_text': 'Longitude:',
               'name': 'inputLon',
-              'placeholder': site.longitude}
+              'placeholder': lon}
 
     
     # Pass variables to the template via the context dictionary
@@ -86,12 +102,12 @@ def snow_graph(request):
     lon = '15'
 
     #Check form data
-    if request.POST and 'geometry' in request.POST:
-        geom = request.POST['geometry']
-        data = json.loads(geom)
-        coords = data['geometries'][0]['coordinates']
-        lat = coords[0]
-        lon = coords[1]
+    #if request.POST and 'geometry' in request.POST:
+    #    geom = request.POST['geometry']
+    #    data = json.loads(geom)
+    #    coords = data['geometries'][0]['coordinates']
+    #    lat = coords[0]
+    #    lon = coords[1]
 
     if request.GET:
         lat = request.GET['inputLat']
@@ -101,12 +117,25 @@ def snow_graph(request):
         endDate2 = datetime.datetime.strptime(endDate, '%Y-%m-%d')
         startDate2 = (endDate2 - datetime.timedelta(days=numdays)).strftime("%Y-%m-%d")
 
+        # Saving the last-selected site to the DB
+        session = SessionMaker()
+    
+        #Query DB for gage object
+        id = 1
+        site = session.query(SnowSite).filter(SnowSite.id==id).one()
+        site.latitude = lat
+        site.longitude = lon
+        session.commit()
+
         #Make the waterml url query string
         waterml_url = '?start=%s&end=%s&lat=%s&lon=%s' % (startDate2, endDate, lat, lon)
 
+        #Make the map url query string
+        map_url = '?days=%s&end=%s&lat=%s&lon=%s' % (numdays, endDate, lat, lon)
+
     
     #Create template context dictionary
-    context = {'lat':lat, 'lon':lon, 'startDate':startDate2, 'endDate': endDate, 'waterml_url': waterml_url}
+    context = {'lat':lat, 'lon':lon, 'startDate':startDate2, 'endDate': endDate, 'waterml_url': waterml_url, 'map_url': map_url}
 
     return render(request, 'snow_inspector/snow_graph.html', context)
 
